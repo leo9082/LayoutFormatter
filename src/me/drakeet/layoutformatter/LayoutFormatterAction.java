4e298417fe7591ce42d6d70da87c95fd24bb694c
drakeet <drakeet.xxf@cainiao.com>
2016-04-24 19:18:10 +0800

    Adjust the position of Action to RefactoringMenu; Support Undo.

--- src/me/drakeet/layoutformatter/LayoutFormatterAction.java
+++ src/me/drakeet/layoutformatter/LayoutFormatterAction.java
@@ -5,7 +5,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.IdeActions;
 import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
-import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
@@ -14,6 +14,7 @@ import com.intellij.openapi.ui.Messages;
  * Created by drakeet on 16/4/8.
  */
 public class LayoutFormatterAction extends AnAction {
+
     public LayoutFormatterAction() {
         // Set the menu item name.
         super("Layout _Formatter _Action");
@@ -24,15 +25,15 @@ public class LayoutFormatterAction extends AnAction {
         final Project project = event.getData(PlatformDataKeys.PROJECT);
         if (event.getData(LangDataKeys.EDITOR) != null) {
             final Document document = event.getData(LangDataKeys.EDITOR).getDocument();
-            ApplicationManager.getApplication().runWriteAction(new Runnable() {
-                @Override public void run() {
+            new WriteCommandAction.Simple(project) {
+                @Override protected void run() throws Throwable {
                     String txt = document.getText();
                     document.setText(Formatter.apply(txt));
                     event.getActionManager()
                          .getAction(IdeActions.ACTION_EDITOR_REFORMAT)
                          .actionPerformed(event);
                 }
-            });
+            }.execute();
         } else {
             Messages.showMessageDialog(project, "Please focus in your editor and try again.",
                     "Error", null);
