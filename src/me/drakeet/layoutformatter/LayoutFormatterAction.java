9c3cf19dee6c255d31173a17d8eb2e75fe97c38b
drakeet <drakeet.xxf@cainiao.com>
2016-04-24 19:28:16 +0800

    Filter non XML file.

--- src/me/drakeet/layoutformatter/LayoutFormatterAction.java
+++ src/me/drakeet/layoutformatter/LayoutFormatterAction.java
@@ -2,13 +2,16 @@ package me.drakeet.layoutformatter;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.IdeActions;
 import com.intellij.openapi.actionSystem.LangDataKeys;
-import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Created by drakeet on 16/4/8.
@@ -22,9 +25,10 @@ public class LayoutFormatterAction extends AnAction {
 
 
     public void actionPerformed(final AnActionEvent event) {
-        final Project project = event.getData(PlatformDataKeys.PROJECT);
-        if (event.getData(LangDataKeys.EDITOR) != null) {
-            final Document document = event.getData(LangDataKeys.EDITOR).getDocument();
+        final Project project = getEventProject(event);
+        Editor editor = event.getData(LangDataKeys.EDITOR);
+        if (editor != null) {
+            final Document document = editor.getDocument();
             new WriteCommandAction.Simple(project) {
                 @Override protected void run() throws Throwable {
                     String txt = document.getText();
@@ -38,5 +42,17 @@ public class LayoutFormatterAction extends AnAction {
             Messages.showMessageDialog(project, "Please focus in your editor and try again.",
                     "Error", null);
         }
+    }
+
+
+    @Override public void update(AnActionEvent event) {
+        super.update(event);
+        final VirtualFile file = CommonDataKeys.VIRTUAL_FILE.getData(event.getDataContext());
+        event.getPresentation().setVisible(isXmlFile(file));
+    }
+
+
+    private static boolean isXmlFile(@Nullable VirtualFile file) {
+        return file != null && file.getName().endsWith(".xml");
     }
 }
